c9408f349f18446c11ee88e004eef763f7e1ba87
Johnny Perez <johnny.perez2@northwestern.edu>
2017-05-20 10:18:20 -0500

    added userlogin table

--- models/userprofile.js
+++ models/userprofile.js
@@ -15,7 +15,8 @@ module.exports = function(sequelize, DataTypes) {
 	intensity: DataTypes.INTEGER,
 	perception: DataTypes.STRING,
 	goal: DataTypes.STRING,
-    membership: DataTypes.STRING
+    membership: DataTypes.STRING,
+    profileScore: DataTypes.INTEGER
   });
   return userProfile;
 };
